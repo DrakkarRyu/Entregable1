aef51b1f4b9c1af67feff8146884d9650b2a3cb1
DrakkarRyu <javisodst-metal@outlook.com>
2022-02-27 18:33:24 -0600

    Icono

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -3,14 +3,14 @@ import Button from "./Button";
 
 
 
-function QuoteBox({quote, author,background,handleQuote}) {
+function QuoteBox({ quote, author, background, handleQuote }) {
     return (
         <div className='quoteBox'>
             <h2 style={{ color: background }}>{quote}</h2>
             <h3 style={{ color: background }}>{author}</h3>
-          <Button handleQuote={handleQuote} background={background}/>
+            <Button handleQuote={handleQuote} background={background} />
         </div>
-        
+
     );
 }
 
